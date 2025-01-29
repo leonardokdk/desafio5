987c8f2c54363a95f63496888eb126292a8839e8
leonardokdk <ssleonardo29@gmail.com>
2025-01-28 21:47:54 -0400

    Segundo commit

--- Main.java
+++ Main.java
@@ -2,11 +2,11 @@ package Desafio5;
 
 public class Main {
     public static void main(String[] args) {
-        ContaBancaria leonardo = new ContaCorrente(1000);
-        ContaBancaria fernando = new ContaPoupanca(1000);
+        ContaBancaria leonardo = new ContaCorrente(2000);
+        ContaBancaria fernando = new ContaPoupanca(2000);
 
-        leonardo.depositar(1000);
-        fernando.depositar(1000);
+        leonardo.depositar(100);
+        fernando.depositar(100);
 
         System.out.println("Saldo Conta Corrente: " + leonardo.consultarSaldo());
         System.out.println("Saldo Conta Corrente: " + fernando.consultarSaldo());
@@ -14,8 +14,8 @@ public class Main {
         BancoKonoha bradesco = new BancoKonoha();
         bradesco.transferencia(leonardo, fernando, 500);
 
-        System.out.println("Saldo conta corrente depois da transferencia: " + fernando.consultarSaldo());
-        System.out.println("Saldo conta corrente depois da transferencia: " + fernando.consultarSaldo());
+        System.out.println("Saldo conta corrente depois da transferencia: " + leonardo.consultarSaldo());
+        System.out.println("Saldo conta poupança depois da transferencia: " + fernando.consultarSaldo());
 
     }
 }
